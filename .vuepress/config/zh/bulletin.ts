1dd6a1d5d69f2fe8f36fe2054b991fdb3e414903
rqh656418510 <656418510@qq.com>
2023-07-30 23:51:28 +0800

    优化静态资源的引用

--- .vuepress/config/zh/bulletin.ts
+++ .vuepress/config/zh/bulletin.ts
@@ -19,7 +19,7 @@ export const bulletin = {
       children: [
         {
           text: '打赏',
-          link: '/docs/other/donate.html'
+          link: '/xiangqi-blog/docs/other/donate.html'
         }
       ]
     }
